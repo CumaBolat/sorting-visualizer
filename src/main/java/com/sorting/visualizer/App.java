8df278bbac053efbb510858c5b91696c4ace9a13
Cuma Bolat <cuma.bolat@toptal.com>
2025-05-01 23:09:34 +0300

    develop: initial frame

--- src/main/java/com/sorting/visualizer/App.java
+++ src/main/java/com/sorting/visualizer/App.java
@@ -1,10 +1,14 @@
 package com.sorting.visualizer;
 
-/**
- * Hello world!
- */
+import javax.swing.SwingUtilities;
+
+import com.sorting.visualizer.frontend.SortingVisualizer;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+      SortingVisualizer app = new SortingVisualizer();
+      app.setVisible(true);
+    });
+  }
 }
---
+++ src/main/java/com/sorting/visualizer/frontend/SortingVisualizer.java
@@ -0,0 +1,12 @@
+package com.sorting.visualizer.frontend;
+
+import javax.swing.JFrame;
+
+public class SortingVisualizer extends JFrame {
+  public SortingVisualizer() {
+    setTitle("Sorting Algorithm Visualizer");
+    setSize(800, 600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+  }
+}
